7ab64d6fbcb681c2397931e776fb1ca0c7995239
iamutkarshyadav10 <83831071+iamutkarshyadav10@users.noreply.github.com>
2021-05-07 22:08:30 +0530

    Add files via upload

---
+++ Sum2.java
@@ -0,0 +1,37 @@
+
+
+/*import java.util.*;
+
+
+
+class Sum2
+ {
+      public static void main(String args [])
+    {
+      Scanner sc=new Scanner (System.in);
+	  
+      System.out.print("Enter a First no.");
+      int a= sc.nextInt();
+	  
+	  System.out.print("Enter a second no.");
+	  int b= sc.nextInt();
+     
+	  int c= a+b;
+	   System.out.println("Addtion :"+c);
+	}
+ }
+*/ 
+ 
+ class Sum2
+ {
+      public static void main(String args [])
+    {
+     int a= 74;
+	 int b= 36;
+	 int c= a+b;
+	 
+	 System.out.println("Addtion 74+36="+c);
+	  
+	  
+	}
+ }	
